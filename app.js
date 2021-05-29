eac0240a454d2fc03234d1f3017a3f986d2d47aa
hari1016 <63118506+hari1016@users.noreply.github.com>
2021-05-29 17:36:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app= express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/Touride',{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopolgy:true,
+})
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{console.log("Database connected:")});
+
+app.use( express.static(path.join(__dirname,'public' )));
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+
+const reviewSchema =new mongoose.Schema({
+    rating : Number,
+    review : String,
+});
+const Review = mongoose.model('Review',reviewSchema);
+
+
+app.get('/goldentemple', async (req,res) =>{
+    const reviews = await Review.find();
+    res.render("GoldenTemple-Info",{reviews});
+})
+
+app.post('/rating', async (req,res) => {
+    const newReview = new Review(req.body);
+    await newReview.save(); 
+    res.redirect('/goldentemple');
+})
+
+
+app.listen(4000,() =>{
+    console.log("serving on port 4000");
+})
